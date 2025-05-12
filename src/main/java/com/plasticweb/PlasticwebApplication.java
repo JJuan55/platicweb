169b961aaa16361c6f1eba37ee9827f61aea83c6
JJuan55 <jcdavidcito@gmail.com>
2025-05-12 00:17:48 -0500

    Postgress

--- src/main/java/com/plasticweb/PlasticwebApplication.java
+++ src/main/java/com/plasticweb/PlasticwebApplication.java
@@ -3,11 +3,14 @@ package com.plasticweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal que inicia la aplicación Spring Boot.
+ */
 @SpringBootApplication
-public class PlaticwebApplication {
+public class PlasticwebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PlaticwebApplication.class, args);
+        SpringApplication.run(PlasticwebApplication.class, args);
     }
-
 }
+
